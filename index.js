0dfff8b46c4c0245ecc557866fa0e0da584f8de7
Matt Edelman <medelman@paypal.com>
2014-05-28 09:10:52 -0700

    adding travis integration and basic unit test

--- index.js
+++ index.js
@@ -2,31 +2,30 @@
 │  Copyright (C) 2014 eBay Software Foundation                                │
 │                                                                             │
 │                                                                             │
-│   Licensed under the Apache License, Version 2.0 (the "License"); you may   │
+│   Licensed under the Apache License, Version 2.0 (the 'License'); you may   │
 │   not use this file except in compliance with the License. You may obtain   │
 │   a copy of the License at http://www.apache.org/licenses/LICENSE-2.0       │
 │                                                                             │
 │   Unless required by applicable law or agreed to in writing, software       │
-│   distributed under the License is distributed on an "AS IS" BASIS,         │
+│   distributed under the License is distributed on an 'AS IS' BASIS,         │
 │   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  │
 │   See the License for the specific language governing permissions and       │
 │   limitations under the License.                                            │
 \*───────────────────────────────────────────────────────────────────────────*/
-/* global module: true, require: true, console: true */
-"use strict";
-var async = require("async");
+/* global module: true */
+'use strict';
 module.exports = {
-	"setup": function (config, result, callback) {
+	'setup': function (config, result, callback) {
 		var returnObj = result;
-		returnObj.locatex = (function locatex(_locator) {
-			var locale = (result.props && result.props.locale) ? result.props.locale : "default",
+		returnObj.locatex = function locatex(_locator) {
+			var locale = (result.props && result.props.locale) ? result.props.locale : 'default',
 				locatr = this.locator;
-			_locator.split(".").forEach(function (level) {
+			_locator.split('.').forEach(function (level) {
 				locatr = locatr[level];
 			});
-			return locatr[locale] || locatr["default"] || locatr;
+			return locatr[locale] || locatr['default'] || locatr;
 
-		}).bind(result);
+		}.bind(result);
 		callback(null, config, returnObj);
 
 	}
